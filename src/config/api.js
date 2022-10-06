47af2c884f210c8d101a639bc0148bae424954bd
Rodrigo Dias <rodrigovicente.dev@gmail.com>
2022-10-06 16:15:02 -0300

    alteracao

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:4000",
-  production: "",
+  production: "https://proj3ironhack.herokuapp.com",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
